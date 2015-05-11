4035f3e872282c10478894f6cd97af06fde8e9a2
VICI <vici@VICI.local>
2015-05-11 21:04:06 +0800

    add binary

--- src/Day20150508/AddBinary.java
+++ src/Day20150508/AddBinary.java
@@ -17,6 +17,11 @@ public class AddBinary {
 
 	}
 
+	/*
+	 * 从后向前面迭代，迭代max(a.length,b.length)次，在第i次迭代中，a中的字符为
+	 * a[a.length - i - i],b中的字符为b[b.length - 1- i]ß
+	 * 
+	 */
 	public String addBinary(String a, String b) {
         if(a == null && b == null)
         	return "";
@@ -26,8 +31,45 @@ public class AddBinary {
         if(b == null)
         	return a;
         
-        int len = Math.max(a.length(), b.length());
+        char[] aa = a.toCharArray();
+        char[] bb = b.toCharArray();
+        int carry = 0;;
+        int len = Math.max(aa.length, bb.length);    
+        int[] res = new int[len];
+        int pos = len - 1;
         
+        for(int i = 0; i < len; i ++){
+        	if(aa.length - 1 - i >= 0 && bb.length - 1 - i >= 0){
+        		int c1 = aa[aa.length - 1 -i] - '0'; 
+        		int c2 = bb[bb.length -1 - i] - '0';
+        		
+        		res[pos --] = (carry + c1 + c2) % 2;
+        		carry = (carry + c1 + c2) / 2;
+        	}
+        	else if(aa.length - 1 - i >= 0){
+        		int c1 = aa[aa.length - 1- i] - '0';
+        		res[pos --] = (carry + c1 ) % 2;
+        		carry = (carry + c1) / 2;
+        	}
+        	else if(bb.length - 1- i >= 0){
+        		int c2 = bb[bb.length - 1- i] - '0';
+        		res[pos --] = (carry + c2 ) % 2;
+        		carry = (carry + c2) / 2;
+
+        	}
+        	else{
+        		continue;
+        	}
+        	
+        }
         
+        StringBuilder sb = new StringBuilder();
+        if(carry > 0)
+        	sb.append('1');
+        
+        for(int i = 0; i < res.length; i ++)
+        	sb.append(res[i]);
+        
+        return sb.toString();
     }
 }
